49425a3019ca4532ebea4fbff5eefbeca8469404
luisflm99 <121656660+luisflm99@users.noreply.github.com>
2023-11-16 22:14:31 -0500

    felix2

--- static/js/order.js
+++ static/js/order.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     "use strict";
-    document.querySelector('.btn-optimized').addEventListener('click', function() {
-        let button = this;
-        button.innerHTML = `
-        <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-        Optimizando...`;
-        button.disabled = true;
+    document.querySelectorAll('.btn-optimized').forEach((button) => {
+        button.addEventListener('click', function() {
+            this.innerHTML = `
+            <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+            Optimizando...`;
+            this.disabled = true;
+        });
     });
 });
